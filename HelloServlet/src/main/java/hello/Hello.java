cb69b47da91f7e82b38c522efa0d1c75fba84e05
evanalves <49462793+evanalves@users.noreply.github.com>
2023-08-21 11:46:40 -0300

    Css/JDBC

---
+++ HelloServlet/src/main/java/hello/Hello.java
@@ -0,0 +1,46 @@
+package hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<!doctype html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello Servlet</title>");
+		out.println("<body>");
+		out.println("<h1>Hello Servlet</h1>");
+		out.println("professor");
+		out.println("</body>");
+		out.println("</html>");
+		
+
+	}
+
+}
